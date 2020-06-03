c439a88dca24eb9c642870d826f8c874b31169c0
Christian Bremer <bremersee@googlemail.com>
2020-06-03 14:08:02 +0200

    Init

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -23,4 +23,8 @@ fi
 if [ -z "$SCS_USER_PASSWORD" ] && [ ! -z "$SCS_USER_PASSWORD_FILE" ] && [ -e $SCS_USER_PASSWORD_FILE ]; then
   export SCS_USER_PASSWORD="$(cat $SCS_USER_PASSWORD_FILE)"
 fi
+echo "Application: $APPLICATION_NAME"
+echo "Pattern: $SCS_PATTERN"
+echo "Location: $SCS_CONTENT_LOCATION"
+echo "Index: $SCS_INDEX"
 java -Djava.security.egd=file:/dev/./urandom -jar /opt/app.jar
